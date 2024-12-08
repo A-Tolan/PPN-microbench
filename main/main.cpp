802900ed75c67614f4e3805946cacd3fbc38f717
A-Tolan <andreea.tolan@ens.uvsq.fr>
2024-12-08 22:30:53 +0100

    Add of memoryBenchmark in main and Memory in conductor_main.

--- main/main.cpp
+++ main/main.cpp
@@ -2,11 +2,15 @@
 #include <PPN-microbench/microbench.hpp>
 #include <PPN-microbench/ops/flops.hpp>
 #include <PPN-microbench/ops/iops.hpp>
+#include <PPN-microbench/memory.hpp>
 
 #include <nlohmann/json.hpp>
 #include <vector>
 
 int main() {
-    Flops f(10);
-    f.context.getInstance().getJson();
+    Memory memoryBenchmark("Memory Latency Benchmark", 1);
+    memoryBenchmark.run();
+    json results = memoryBenchmark.getJson();
+    std::cout << results.dump(4) << std::endl;
+    return 0;
 }
